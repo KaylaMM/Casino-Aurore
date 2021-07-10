b87031c033e6811fdda2dee81fbc7bb86b9203e7
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-07-10 00:04:36 -0400

    saving final changes. ready to submit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-function ageGate() {}
+jQuery(document).ready(function ($) {
+  if (sessionStorage.getItem("saveAge") !== "true") {
+    $(".ag-module").show();
+  } else {
+    $(".ag-module").hide();
+    $(".overlay-verify").hide();
+  }
+
+  $(".btn-yes").click(function () {
+    sessionStorage.setItem("saveAge", "true");
+    $(".ag-module").hide();
+    $(".overlay-verify").hide();
+  });
+
+  $(".btn-no").click(function () {
+    window.alert("Sorry, you must be 21 or older to visit our site");
+  });
+});
